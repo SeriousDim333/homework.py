518770d25cb4ff2f5fff632b3da1beb677fe04f9
Dmitriy <seriousdim@mail.ru>
2023-03-17 13:17:44 +0300

    добавил решение 2 задачи 3 семинара

---
+++ sem3/task2.py
@@ -0,0 +1,23 @@
+# Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X. 
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+# *Пример:*
+# 5
+# 1 2 3 4 5
+# 6
+# -> 5
+
+from random import randint
+
+element_amount = int(input("введите количество элементов в массиве: "))
+numbers = [randint(0,100) for _ in range(element_amount)]
+print(numbers)
+
+num_search = int(input("какое число ищем? "))
+min = 1000
+temp = 0
+for i in numbers:
+    if abs(i - num_search)< min:
+        min = abs(i - num_search)
+        temp  = i
+print(f"самое близкое {temp}")
